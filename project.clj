5d875b96b4e49034fb0f90ce3701770cba43d1e5
Jason Murphy <json.murphy@gmail.com>
2016-07-18 10:06:29 -0500

    Bumped Conskit and trapperkeeper version

--- project.clj
+++ project.clj
@@ -4,11 +4,12 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [conskit "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[puppetlabs/trapperkeeper "1.3.0" :classifier "test"]
-                                  [puppetlabs/kitchensink "1.3.0" :classifier "test" :scope "test"]
-                                  [midje "1.8.3"]]
+                 [conskit "0.2.0-SNAPSHOT"]]
+  :profiles {:dev {:source-paths ["dev" "bidi"]
+                   :dependencies [[puppetlabs/trapperkeeper "1.4.1" :classifier "test"]
+                                  [puppetlabs/kitchensink "1.3.1" :classifier "test" :scope "test"]
+                                  [midje "1.8.3"]
+                                  [bidi "1.25.1"]]
                    :plugins [[lein-midje "3.2"]]}
              :bidi-routing {:source-paths ["bidi"]
                             :dependencies [[bidi "1.25.1"]]}}
